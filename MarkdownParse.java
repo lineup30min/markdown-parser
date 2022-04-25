f593201b77913593b23cdde4a06ede81e548b0c8
Sammy <xshe@ucsd.edu>
2022-04-24 20:14:15 -0700

    Code change for test-file-1.md
    The expected output is:
    [https://s()mething.com, some-thing.html]
    But, the failure one is:
    [https://s(, some-thing.html]

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,16 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+       
+            // Code change for test-file-1.md
+            String checkingForError = markdown.substring(closeParen + 1);
+            int PotenParen = markdown.indexOf(")", closeParen + 1);
+            if (checkingForError.contains(")") && 
+                checkingForError.indexOf(")", 0) < 
+                checkingForError.indexOf("[", 0)) {
+                closeParen = PotenParen;
+            }
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
